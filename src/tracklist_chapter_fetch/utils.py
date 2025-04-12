7a2ba253454a478c58b3c39e0ad129a6634dcb21
Martin Tracey <matracey@outlook.ie>
2025-04-12 15:53:15 -0700

    feat: add utility function for logging configuration

---
+++ src/tracklist_chapter_fetch/utils.py
@@ -0,0 +1,29 @@
+"""
+Utility functions for Tracklist Chapter Fetch.
+"""
+
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+
+def setup_logging(verbose: bool = False, quiet: bool = False) -> None:
+    """
+    Configure logging level based on command line arguments.
+
+    Args:
+        verbose: Set to True for verbose logging (DEBUG level)
+        quiet: Set to True to suppress all but error messages
+    """
+    if quiet:
+        logger.setLevel(logging.ERROR)
+    elif verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
