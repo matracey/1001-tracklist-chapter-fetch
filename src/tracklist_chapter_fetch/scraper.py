99ded90a8be12b4ad5d439c126aa7e8b0f4caa12
Martin Tracey <matracey@outlook.ie>
2025-04-12 16:09:48 -0700

    feat: implement TracklistScraper class and ScrapingError exception class

---
+++ src/tracklist_chapter_fetch/scraper.py
@@ -0,0 +1,18 @@
+"""
+Web scraping functionality for extracting tracklists from 1001tracklists.com.
+"""
+
+
+class ScrapingError(Exception):
+    """Exception raised for scraping errors."""
+
+
+class TracklistScraper:
+    """
+    Scraper for 1001tracklists.com to extract tracklist information.
+    """
+
+    def __init__(self):
+        """
+        Initialize the scraper.
+        """
